42e61ecfa1f3d13d05cbd08ed6aac859b6bb47fb
Ially Fabiane da Silva <94318317+IallyFabiane@users.noreply.github.com>
2023-03-08 08:02:33 -0300

    usando data-attributes

--- projeto_inicial/listaDeTarefas.js
+++ projeto_inicial/listaDeTarefas.js
@@ -1,4 +1,17 @@
 const novaTarefa = document.querySelector('[data-form-button]');
-novaTarefa.addEventListener('click', () => {
-    console.log('Fui clicado');
-})
+
+const criaraTarefa = (event) => {
+    event.preventDefault(); 
+    const input = document.querySelector('[data-form-input');
+    let value = input.value;
+    console.log(value);
+
+    const tarefa = document.querySelector('[data-task');
+    const conteudo = `<p class='content'>${value}</p>`;
+
+    tarefa.innerHTML = conteudo;
+    input.value = '';
+}
+
+
+novaTarefa.addEventListener('click', criaraTarefa);
